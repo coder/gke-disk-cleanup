6da806e7ef675814cfa34767a15826c580b6e3c7
Cian Johnston <public@cianjohnston.ie>
2022-04-01 10:32:50 +0100

    add description and labels of original disk to snapshots

--- cmd/gke-disk-cleanup/main.go
+++ cmd/gke-disk-cleanup/main.go
@@ -288,13 +288,19 @@ func doCleanupOne(ctx context.Context, dc disksClient, di diskIterator, projectI
 			log.Info().Str("diskName", disk.GetName()).Int64("sizeGB", disk.GetSizeGb()).Str("lastAttachTime", disk.GetLastAttachTimestamp()).Str("labels", fmt.Sprintf("%+v", diskLabels)).Msg("snapshotting disk prior to deletion")
 			reqID := uuid.New()
 			snapName := fmt.Sprintf("%s-snapshot", disk.GetName())
+			diskLabels := disk.GetLabels()
+			if diskLabels == nil {
+				diskLabels = make(map[string]string)
+			}
+			diskLabels["created-by"] = "gke-disk-cleanup"
 			req := &computepb.CreateSnapshotDiskRequest{
 				Disk:      disk.GetName(),
 				Project:   projectID,
 				RequestId: pointer.String(reqID.String()),
 				SnapshotResource: &computepb.Snapshot{
 					Name:             &snapName,
-					Labels:           map[string]string{"created-by": "gke-disk-cleanup"},
+					Description:      pointer.String(disk.GetDescription()),
+					Labels:           diskLabels,
 					StorageLocations: []string{disk.GetRegion()},
 				},
 				Zone: zone,
